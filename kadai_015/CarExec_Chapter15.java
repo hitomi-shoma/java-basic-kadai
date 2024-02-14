d175bd255335ad365377684559a2279e59c246eb
hitomi-shoma <151494818+hitomi-shoma@users.noreply.github.com>
2024-02-14 13:33:25 +0900

    Add files via upload
    課題提出後の修正

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -4,11 +4,19 @@ public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
 		// インスタンス化
-				Car_Chapter15 car = new Car_Chapter15(1,10);
-				// ギアの値により速度を変える(gearChange)(引数：int afterGear)
-				car.gearChange(3);
-				//	速度変更
-				car.run();
+		Car_Chapter15 car = new Car_Chapter15(1,10);
+		// ギアの値により速度を変える(gearChange)(引数：int afterGear)		
+		
+		for(int i = 2; i <= 5; i++) {	
+			car.gearChange(i);
+			car.run();
+		}
+		System.out.println();
+		
+		for( int i = 4; i >= 1; i--) {
+			car.gearChange(i);
+			car.run();
+		}
 
 	}
 
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -3,9 +3,8 @@ package kadai_015;
 public class Car_Chapter15 {
 
 	// フィールド（内部データ）
-	private int gear = 1;
-	private int speed = 10;
-	
+	private int gear;
+	private int speed;
 	
 	//	コンストラクタ(初期化)
 	public Car_Chapter15( int gear,int speed ) {
@@ -14,26 +13,25 @@ public class Car_Chapter15 {
 	}
 	
 	// メソッド：
-	public void gearChange ( int afterGear ) {
-		
+	public void gearChange( int afterGear ) {
 		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
 		
+		this.gear = afterGear;
 	//	switch文で条件分岐させる
-		switch (afterGear) {
+		switch (this.gear) {
 		
-		case 1 -> speed=10;
-		case 2 -> speed=20;
-		case 3 -> speed=30;
-		case 4 -> speed=40;
-		case 5 -> speed=50;
-		default -> speed=10;
+		case 1 -> speed = 10;
+		case 2 -> speed = 20;
+		case 3 -> speed = 30;
+		case 4 -> speed = 40;
+		case 5 -> speed = 50;
+		default -> speed = 10;
 		
 		}
 	}
 	
-public void run () {
-		
-		System.out.println("速度は時速" + speed + "kmです");
+	public void run() {
+		System.out.println("ギアは" + gear + "、速度は時速" + speed + "kmです");
 	}
 	
 }
