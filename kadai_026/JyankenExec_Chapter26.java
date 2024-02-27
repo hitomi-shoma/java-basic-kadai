7a862f7d441cce60c038d7c1475c8df32c8d512b
hitomi-shoma <151494818+hitomi-shoma@users.noreply.github.com>
2024-02-27 15:13:38 +0900

    Add files via upload
    kadai_026

---
+++ kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,12 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+
+	public static void main(String[] args) {
+		//じゃんけんクラスのインスタンスを生成
+		Jyanken_Chapter26 jyanken = new Jyanken_Chapter26();
+		//じゃんけんの勝敗の結果を出力
+		jyanken.playGame();
+	}
+
+}
